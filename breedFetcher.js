745683326b7684238ccd4f717ff60b17c28c0f28
danschae <danschae@gmail.com>
2020-09-24 18:42:01 +0000

    done

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,15 +1,20 @@
 const request = require('request');
-const args = process.argv.slice(2);
-let url = ` https://api.thecatapi.com/v1/breeds/search?q=${args}`;
 
-request(url, (error, response, body) => {
-  if (error) {
-    return console.log("error", error);
-  } 
-  const data = JSON.parse(body);
-   if (data[0] === undefined) {
-    return console.log("breed not found")
-  } else {
-    return console.log(data[0]);
-  }
-});
+const fetchBreedDescription = function(breedName, callback) {
+  let url = ` https://api.thecatapi.com/v1/breeds/search?q=${breedName}`;
+  request(url, (error, response, body) => {
+    if (error) {
+      return callback(error, null)
+    } 
+    const data = JSON.parse(body);
+     if (data[0] === undefined) {
+      return callback("breed not found")
+    } else {
+      return callback(null, data[0].description);
+    }
+  });
+};
+
+module.exports = { 
+  fetchBreedDescription
+ };
